d874b16841ac7dbd83e82c50fb1cfb417f50b50d
fischkoder <vielenfisch212@gmail.com>
2022-04-04 21:32:59 +1000

    update

--- product.php
+++ product.php
@@ -8,9 +8,22 @@
     <body>
         <?php
             $id = $_GET['id'];
-            print $id;
-            $db_link = mysqli_connect('localhost','root','mysql','grocery');
-
+            $db_link = mysqli_connect('localhost','root','mysql','grocery') or die("Error Connection");
+            $query_string = "select * from products where product_id = $id;";
+            $db_result = mysqli_query($db_link,$query_string);
+            $num_rows = mysqli_num_rows($db_result); 
+            print "<img src='images/$id.jpg'>";
+            if($num_rows > 0){
+                while($row = mysqli_fetch_assoc($db_result)){
+                    print "<ul>";
+                    print "<li>Product ID: ".$row['product_id']."</li>";
+                    print "<li>Name: ".$row['product_name']."</li>";
+                    print "<li>Unit Price: ".$row['unit_price']."</li>";
+                    print "<li>Specs: ".$row['unit_quantity']."</li>";
+                    print "<li>In Stock: ".$row['in_stock']."</li>";
+                    print "</ul>";
+                }
+            }
         ?>
     </body>
 </html>
